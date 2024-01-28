e1f4b5b981874256d6a70fab1f5871ffbb97a5d8
acdundore <78836266+acdundore@users.noreply.github.com>
2024-01-28 17:18:42 -0600

    feat: log_scaling for varibles
    added the ability to use logarithmic scaling for certain variables; also updated documentation to reflect this

--- docs/variables.md
+++ docs/variables.md
@@ -12,8 +12,8 @@ When defining variables and search space bounds for an algorithm, the user passe
 
 | Variable Type | Class Instantiation |
 |---|---|
-| Floats/Continuous | optiseek.variables.**var_float**(*var_name, bounds*) |
-| Integers | optiseek.variables.**var_int**(*var_name, bounds*) |
+| Floats/Continuous | optiseek.variables.**var_float**(*var_name, bounds, log_scale=False*) |
+| Integers | optiseek.variables.**var_int**(*var_name, bounds, log_scale=False*) |
 | Categorical | optiseek.variables.**var_categorical**(*var_name, choices*) |
 | Boolean (True/False) | optiseek.variables.**var_bool**(*var_name*) |
 
@@ -26,6 +26,7 @@ When defining variables and search space bounds for an algorithm, the user passe
 | var_name : *string* | Name of the variable. This will be used to track output and label the<br/> stored results if applicable. |
 | bounds : *list of floats/int* | A list containing a lower and upper bound for the search space of the<br/> variable in the format *[lower, upper]*. The values can be integers/floats<br/> for `var_float` and must be integers for `var_int`. |
 | choices : *list* | Contains a list of choices to be passed as an argument for the objective<br/> function. The list items may be any type that the objective function<br/> can accept. |
+| log_scale : *boolean* | A flag to toggle on logarithmic scaling for a specified variable. For<br/> example, for a continuous variable with search bounds \[1, 100], the<br/> midpoint without log scaling is 50.5. If `log_scale` was set to `True`, the<br/> distance in the search space from 1 to 10 would be equivalent to that<br/> from 10 to 100. In order to use `log_scale`, the search space for that<br/> variable must be positive. |
 
 ---
 
--- optiseek/variables.py
+++ optiseek/variables.py
@@ -2,17 +2,22 @@ import numpy as np
 
 
 class _variable:
-    def __init__(self, var_name, var_type, bounds, choices, internal_bounds):
+    def __init__(self, var_name, var_type, bounds, choices, log_scale, internal_bounds):
         # initialize variables
         self.var_name = var_name
         self._var_type = var_type
         self.bounds = bounds
         self.choices = choices
+        self.log_scale = log_scale
         self._internal_bounds = np.array(internal_bounds) # convert to array
 
         # calculate parameters to transform from specified to internal bounds
-        self._scale = (self.bounds[1] - self.bounds[0]) / (self._internal_bounds[1] - self._internal_bounds[0])
-        self._shift = -self._scale * self._internal_bounds[0] + self.bounds[0]
+        if self.log_scale == False:
+            self._scale = (self.bounds[1] - self.bounds[0]) / (self._internal_bounds[1] - self._internal_bounds[0])
+            self._shift = -self._scale * self._internal_bounds[0] + self.bounds[0]
+        else:
+            self._scale = (np.log10(self.bounds[1]) - np.log10(self.bounds[0])) / (self._internal_bounds[1] - self._internal_bounds[0])
+            self._shift = -self._scale * self._internal_bounds[0] + np.log10(self.bounds[0])
 
     @property
     def var_name(self):
@@ -46,9 +51,28 @@ class _variable:
         else:
             self._choices = value
 
+    @property
+    def log_scale(self):
+        return self._log_scale
+
+    @log_scale.setter
+    def log_scale(self, value):
+        if value == True and np.any(self.bounds <= 0):
+            raise ValueError('Bounds must both be positive to enable log scaling.')
+        if type(value) is not bool:
+            raise TypeError('log_scale must be either True or False.')
+        else:
+            self._log_scale = value
+
     # function to convert a value from internal position to specified position, including encodings for categorical, boolean, and integers
     def _internal_to_specified(self, internal_position):
-        specified_position = self._scale * internal_position + self._shift
+        # convert from the internal coordinate system to the specified system
+        if self.log_scale == False:
+            specified_position = self._scale * internal_position + self._shift
+        else:
+            specified_position = 10 ** (self._scale * internal_position + self._shift)
+
+        # encode the variable if necessary
         if self._var_type in ['categorical', 'bool']:
             encoded_position = self.choices[int(min(max(self.bounds[0], np.floor(specified_position)), self.bounds[1] - 1))]
         elif self._var_type == 'int':
@@ -59,19 +83,19 @@ class _variable:
         return encoded_position
 
 class var_float(_variable):
-    def __init__(self, var_name, bounds):
-        super().__init__(var_name, var_type='float', bounds=bounds, choices=None, internal_bounds=[-1, 1])
+    def __init__(self, var_name, bounds, log_scale=False):
+        super().__init__(var_name, var_type='float', bounds=bounds, choices=None, log_scale=log_scale, internal_bounds=[-1, 1])
 
 class var_int(_variable):
-    def __init__(self, var_name, bounds):
-        super().__init__(var_name, var_type='int', bounds=bounds, choices=None, internal_bounds=[-1, 1])
+    def __init__(self, var_name, bounds, log_scale=False):
+        super().__init__(var_name, var_type='int', bounds=bounds, choices=None, log_scale=log_scale, internal_bounds=[-1, 1])
 
 class var_categorical(_variable):
     def __init__(self, var_name, choices):
         if type(choices) is not list:
             raise TypeError('choices must be a list.')
-        super().__init__(var_name, var_type='categorical', bounds=[0, len(choices)], choices=choices, internal_bounds=[-1, 1])
+        super().__init__(var_name, var_type='categorical', bounds=[0, len(choices)], choices=choices, log_scale=False, internal_bounds=[-1, 1])
 
 class var_bool(_variable):
     def __init__(self, var_name):
-        super().__init__(var_name, var_type='bool', bounds=[0, 2], choices=[False, True], internal_bounds=[-1, 1])
+        super().__init__(var_name, var_type='bool', bounds=[0, 2], choices=[False, True], log_scale=False, internal_bounds=[-1, 1])
